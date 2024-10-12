6b8713c8a128004f7eeff68ccdacf296d0d19637
Jitender Sharma <js903172@gmail.com>
2024-10-12 18:28:48 +0530

    url updated

--- Backend/Frontend/src/components/Login.jsx
+++ Backend/Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://mernbook1.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
